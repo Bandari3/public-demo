c779dfa4e6ce0dc389a8cfe97b0858c252355d0d
saibabu <bandari.saibabu@yahoo.com>
2025-05-21 22:16:34 +0530

    first message

---
+++ src/main/java/com/edureka/may_devops_genai_weekday/App.java
@@ -0,0 +1,13 @@
+package com.edureka.may_devops_genai_weekday;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
